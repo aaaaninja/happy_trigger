5e48ec9d6b4df2a5c0d16e18cc56cc1284d52af1
aaaaninja <keshigomu71505@gmail.com>
2019-08-15 16:21:35 +0900

    :tada: 祝migrate!!
    db/migrateがまた増えたのはrails g devise_toekn_auth:installのせい……
    まぁほぼそのままSchemafileに移植しました (idをuuid指定にしたぐらい)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
+  before_create do
+    self.id = ULID.generate_bytes
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
