4bdd206957b773bdc9201221b5430ad8e1ef86eb
aaaaninja <keshigomu71505@gmail.com>
2019-08-15 17:52:10 +0900

    :tada: modify models and controller!!

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,4 +2,10 @@ class Author < ApplicationRecord
   before_create do
     self.id = ULID.generate_bytes
   end
+  has_many :books
+  validates :name, presence: true
+
+  def to_s
+    name
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,9 @@ class Book < ApplicationRecord
     self.id = ULID.generate_bytes
   end
   belongs_to :author
+  validates :title, presence: true
+
+  def to_s
+    name
+  end
 end
