ef97d65150885a417bbbc8e9acb531ee612a8c7a
aaaaninja <keshigomu71505@gmail.com>
2019-08-15 17:41:19 +0900

    :tada: 新テーブルとmodelをmigrate
    いらないmigrationファイルは削除した

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ApplicationRecord
+  before_create do
+    self.id = ULID.generate_bytes
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ApplicationRecord
+  before_create do
+    self.id = ULID.generate_bytes
+  end
   belongs_to :author
 end
